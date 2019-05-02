f21274224e07649355c44859c6c96a45372c39b2
deepikabhavnani <deepika.bhavnani@arm.com>
2019-05-02 15:27:48 -0500

    Resolve build error without debugging enabled
    In case of non-debug build of maestro (./build.sh) the error was generated:
    ../src/github.com/armPelionEdge/maestro/events/debugging.go:20:8: imported and not used: "fmt"
    
    Resolved by importing `fmt` only in debug mode

--- src/events/debugging.go
+++ src/events/debugging.go
@@ -15,7 +15,7 @@ package events
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import "fmt"
+DEBUG(import "fmt")
 
 DEBUG(func logEvent(format string, a ...interface{}) {)
 DEBUG(s := fmt.Sprintf(format, a...))
