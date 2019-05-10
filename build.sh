a07488fb047ee3524cf0b5249653c766d90556f7
Jimmy Brisson <theotherjimmy@gmail.com>
2019-05-10 15:54:53 -0500

    Use go tags in build.sh to pick debug builds

--- build.sh
+++ build.sh
@@ -26,88 +26,41 @@ THIS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 pushd $THIS_DIR
 
-# you need the -P !! --> http://stackoverflow.com/questions/32477816/safely-replacing-text-with-m4-disabling-new-defines
-
-
-
-# m4 -P macro-setup.m4 -F m4setup.m4f
-
-if [ ! -z "$DEBUG2" ]; then
-	echo "DEBUG2 IS ON!"
-	DEBUG2="-D DEBUG_OUT2=fmt.Printf(\"***DEBUG_GO2:\"+\$@) -D IFDEBUG2=\$1"
-	# if [ "$DEBUG" -ge "1" ]; then
-else
-	DEBUG2="-D DEBUG_OUT2=  -D IFDEBUG2=\$2"
-fi
-
-
-
-if [ ! -z "$DEBUG" ]; then
-	echo "DEBUG IS ON!"
-	PREPROCESS="m4 -P -D IFDEBUG=\$1 -D NODEBUG= -D DEBUG=\$@ macro-setup.m4 -D DEBUG_OUT=fmt.Printf(\"***DEBUG_GO:\"+\$@) ${DEBUG2}"
-	# if [ "$DEBUG" -ge "1" ]; then
-
-	# find
+if [ \( -n "$DEBUG" \) -o \( -n "$DEBUG2" \) ]; then
+  echo "DEBUG ON"
+  GOTAGS="-tags debug"
 	make native.a-debug
 else
-	PREPROCESS="m4 -P -D IFDEBUG=\$2 -D NODEBUG=\$@ -D DEBUG= -D DEBUG_OUT= macro-setup.m4  ${DEBUG2}"
 	make native.a
 fi
 
-#if [ ! -z "$TIGHT" ]; then
-#    GO_LDSWITCH="-ldflags=\"-s -w\""
-#fi
-
-if [ ! -z "$PRETEND" ]; then
-	echo "PRETEND IS ON!"
-fi
-
-
 for f in $(find  src -name '*\.go'); do
-#	echo `basename $f`
 	FILENAME=`basename $f`
 	DIR1=`dirname $f`
 	DIR1=${DIR1#*/}
-#	echo "------"
 	if [ "$DIR1" != "src" ]; then
 		mkdir -p $DIR1
-#		echo "--->"$DIR1"<--"
-#		echo "****"
-#		echo "output: $DIR1/$FILENAME"
 		cp src/$DIR1/$FILENAME $DIR1/$FILENAME
 	else
-#		echo "output: $FILENAME"
 		cp src/$FILENAME $FILENAME
 	fi
 done
 
 for f in $(find  src -name '*\.[ch]'); do
-#	echo `basename $f`
 	FILENAME=`basename $f`
 	DIR1=`dirname $f`
 	DIR1=${DIR1#*/}
-#	echo "------"
 	if [ "$DIR1" != "src" ]; then
 		mkdir -p $DIR1
-#		echo "--->"$DIR1"<--"
-#		echo "****"
-#		echo "output: $DIR1/$FILENAME"
-		# echo $PREPROCESS "src/$DIR1/$FILENAME" "> $DIR1/$FILENAME"
-		# if [ -z "$PRETEND" ]; then $PREPROCESS src/$DIR1/$FILENAME > $DIR1/$FILENAME; fi
 		cp src/$DIR1/$FILENAME $DIR1/$FILENAME
 	else
-#		echo "output: $FILENAME"
-		# echo $PREPROCESS "src/$FILENAME" "> $FILENAME"
-		# if [ -z "$PRETEND" ]; then $PREPROCESS src/$FILENAME > $FILENAME; fi
 		cp src/$FILENAME $FILENAME
 	fi
 done
 
-
 popd
 
 if [ "$1" == "removesrc" ]; then
-#	echo "mv src .src"
 	if [ -d src ]; then
 		mv src .src
 	fi
@@ -125,14 +78,12 @@ sed -i -e "s/BUILD_DATE/${DATE}/g" maestroutils/status.go
 
 if [ "$1" != "preprocess_only" ]; then
 	pushd $GOPATH/bin
+  echo $PWD
 	if [ ! -z "$TIGHT" ]; then
-	    go build -ldflags="-s -w" "$@" github.com/armPelionEdge/maestro/maestro 
+	    go build $GOTAGS -ldflags="-s -w" "$@" github.com/armPelionEdge/maestro/maestro 
 	else
-	    go build "$@" github.com/armPelionEdge/maestro/maestro 
+	    go build $GOTAGS "$@" github.com/armPelionEdge/maestro/maestro 
 	fi
   echo $?
-
-# 
-#go install -x github.com/armPelionEdge/maestro/maestro
 	popd
 fi
