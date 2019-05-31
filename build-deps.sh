c0a3ddf29507608ebbbdad0bd801d23330653039
Cruz Monrreal II <Cruz.Monrreal@arm.com>
2019-05-31 20:58:24 +0000

    Corrected issues found by running 'shellcheck'

--- build-deps.sh
+++ build-deps.sh
@@ -23,28 +23,27 @@ trap 'on_err "${BASH_SOURCE}" "${LINENO}" "${BASH_COMMAND}"' ERR
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  SELF="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 
 THIS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-pushd $THIS_DIR
+pushd "${THIS_DIR}"
 
-cd $THIS_DIR/vendor/github.com/armPelionEdge/greasego/deps/src/greaseLib/deps/libuv-v1.10.1
+cd "${THIS_DIR}/vendor/github.com/armPelionEdge/greasego/deps/src/greaseLib/deps/libuv-v1.10.1"
 
 if [ ! -d build ]; then
     git clone https://chromium.googlesource.com/external/gyp.git build/gyp
 fi
 
-cd $THIS_DIR/vendor/github.com/armPelionEdge/greasego/deps/src/greaseLib/deps
+cd "${THIS_DIR}/vendor/github.com/armPelionEdge/greasego/deps/src/greaseLib/deps"
 ./install-deps.sh
 
-cd $THIS_DIR/vendor/github.com/armPelionEdge/greasego
+cd "${THIS_DIR}/vendor/github.com/armPelionEdge/greasego"
 ./build-deps.sh
 
-cd $THIS_DIR/vendor/github.com/armPelionEdge/greasego
+cd "${THIS_DIR}/vendor/github.com/armPelionEdge/greasego"
 
 # build greasego
 DEBUG=1 ./build.sh
