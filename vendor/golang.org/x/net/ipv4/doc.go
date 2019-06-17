3176ba0e265822023c24a14197502e808bacac79
deepikabhavnani <deepika.bhavnani@arm.com>
2019-06-17 10:12:55 -0500

    Update DNS: miekg/dns post release 1.1.14
    https://github.com/miekg/dns/commit/ee62c8b0866df3cc788d181485c483bf46808a00

--- vendor/golang.org/x/net/ipv4/doc.go
+++ vendor/golang.org/x/net/ipv4/doc.go
@@ -209,7 +209,7 @@
 // LeaveSourceSpecificGroup for the operation known as "include" mode,
 //
 //	ssmgroup := net.UDPAddr{IP: net.IPv4(232, 7, 8, 9)}
-//	ssmsource := net.UDPAddr{IP: net.IPv4(192, 168, 0, 1)})
+//	ssmsource := net.UDPAddr{IP: net.IPv4(192, 168, 0, 1)}
 //	if err := p.JoinSourceSpecificGroup(en0, &ssmgroup, &ssmsource); err != nil {
 //		// error handling
 //	}
@@ -239,7 +239,6 @@
 // IGMPv1 or IGMPv2 and starts to listen to multicast traffic.
 // In the fallback case, ExcludeSourceSpecificGroup and
 // IncludeSourceSpecificGroup may return an error.
-package ipv4
+package ipv4 // import "golang.org/x/net/ipv4"
 
-// BUG(mikio): This package is not implemented on AIX, JS, NaCl and
-// Plan 9.
+// BUG(mikio): This package is not implemented on JS, NaCl and Plan 9.
