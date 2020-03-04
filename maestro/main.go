94d7d8e2adf15fd0696794fa1e8714e9322fe4d5
Michael Ray <michael.ray@arm.com>
2020-03-04 09:55:00 -0600

    Maestro: Remove USRSIG1 listener
    No longer need the USRSIG1 listener to output messages into the logger

--- maestro/main.go
+++ maestro/main.go
@@ -21,8 +21,6 @@ import (
 	"os"
 	"strings"
 	"sync"
-	"os/signal"
-	"syscall"
 	//	"reflect"
 	"github.com/armPelionEdge/greasego"
 	"github.com/armPelionEdge/httprouter"
@@ -58,7 +56,6 @@ import (
 )
 
 var log = logging.MustGetLogger("maestro")
-var fileLog = Log.NewPrefixedLogger("logger")
 
 var (
 	NumWorkers = os.Getenv("MAESTRO_LOG_WORKERS")
@@ -79,22 +76,6 @@ func main() {
 	}
 
 	log.Info("maestro starting.")
-
-	// Debug function that runs on USR1 signal
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGUSR1)
-		for {
-			<-c
-			fmt.Println("[*] SIGUSR1 received")
-			// add all types of log messages here
-			fileLog.Info("debug log output - Info")
-			fileLog.Warn("debug log output - Warn")
-			fileLog.Error("debug log output - Error")
-			fileLog.Debug("debug log output - Debug")
-			fileLog.Success("debug log output - Success")
-        }
-    }()
 
 	configFlag := flag.String("config", "./maestro.config", "Config path")
 	dumpMetaVars := flag.Bool("dump_meta_vars", false, "Dump config file meta variables only")
