d5d6acdc916bad3e0ccb3cb82e1925cb318d6426
J. Michael Welsh <mike.welsh@arm.com>
2020-05-12 05:11:45 +0000

    Clean up
    Cleaned up the code to comply with go Linter

--- logconfig/loggingConfigMonitor.go
+++ logconfig/loggingConfigMonitor.go
@@ -24,34 +24,20 @@ import (
 	"strings"
 
 	"github.com/armPelionEdge/greasego"
-	//"github.com/armPelionEdge/maestro/log"
-
 	"github.com/armPelionEdge/maestro/log"
 	"github.com/armPelionEdge/maestro/maestroConfig"
 	"github.com/armPelionEdge/maestroSpecs"
 )
 
-type LogConfigChangeHook struct {
+// ChangeHook is base struct for our class
+type ChangeHook struct {
 	//struct implementing ConfigChangeHook intf
 }
 
-type ConfigChangeInfo struct {
-	//struct capturing the job args to be carried out when a config change happens
-	configgroup        string      //Config group
-	fieldchanged       string      //Name of the field which is changed
-	canonicalfieldname string      //Canonocal Name(which includes the parent struct name) of the field which is changed
-	futvalue           interface{} //This corresponds to the new value of the changed field
-	curvalue           interface{} //This corresponds to the old/current value of the changed field
-	index              int         //Index of the element if the changed field is an array, otherwise this fied
-}
-
-var configChangeRequestChan chan ConfigChangeInfo = nil
-
-//This is the go routine which waits on configChangeRequestChan and when it
+//ConfigChangeHandler is the go routine which waits on configChangeRequestChan and when it
 //receives a message which is ConfigChangeInfo object, it calls corresponding
 //process functions(see below) based on config group.
 func ConfigChangeHandler(configgroup string, fieldchanged string, futvalue interface{}, curvalue interface{}, index int) {
-	fmt.Printf("in ConfigChangeHandler\n")
 
 	fmt.Printf("ConfigChangeHandler:: group:%s field:%s old:%v new:%v\n", configgroup, fieldchanged, curvalue, futvalue)
 	switch configgroup {
@@ -72,7 +58,7 @@ func ConfigChangeHandler(configgroup string, fieldchanged string, futvalue inter
 
 // ChangesStart is called before reporting any changes via multiple calls to SawChange. It will only be called
 // if there is at least one change to report
-func (cfgHook LogConfigChangeHook) ChangesStart(configgroup string) {
+func (cfgHook ChangeHook) ChangesStart(configgroup string) {
 	fmt.Printf("ConfigChangeHook:ChangesStart: %s\n", configgroup)
 
 	//ConfigChangeHandler(configChangeRequestChan)
@@ -81,7 +67,7 @@ func (cfgHook LogConfigChangeHook) ChangesStart(configgroup string) {
 // SawChange is called whenever a field changes. It will be called only once for each field which is changed.
 // It will always be called after ChangesStart is called
 // If SawChange return true, then the value of futvalue will replace the value of current value
-func (cfgHook LogConfigChangeHook) SawChange(configgroup string, fieldchanged string, futvalue interface{}, curvalue interface{}, index int) (acceptchange bool) {
+func (cfgHook ChangeHook) SawChange(configgroup string, fieldchanged string, futvalue interface{}, curvalue interface{}, index int) (acceptchange bool) {
 	fmt.Printf("ConfigChangeHook:SawChange: %s:%s old:%v new:%v index:%d\n", configgroup, fieldchanged, curvalue, futvalue, index)
 	/*if configChangeRequestChan != nil {
 		fieldnames := strings.Split(fieldchanged, ".")
@@ -97,7 +83,7 @@ func (cfgHook LogConfigChangeHook) SawChange(configgroup string, fieldchanged st
 
 // ChangesComplete is called when all changes for a specific configgroup tagname
 // If ChangesComplete returns true, then all changes in that group will be assigned to the current struct
-func (cfgHook LogConfigChangeHook) ChangesComplete(configgroup string) (acceptallchanges bool) {
+func (cfgHook ChangeHook) ChangesComplete(configgroup string) (acceptallchanges bool) {
 	fmt.Printf("ConfigChangeHook:ChangesComplete: %s\n", configgroup)
 
 	inst := GetInstance()
@@ -120,17 +106,19 @@ func (cfgHook LogConfigChangeHook) ChangesComplete(configgroup string) (acceptal
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Helper functions
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//AddLogFilter is the function to update the live running filters
 func AddLogFilter(filterConfig maestroSpecs.LogFilter) error {
 	fmt.Printf("in AddLogFilter\n")
-	targId := greasego.GetTargetId(filterConfig.Target)
-	if targId == 0 {
+	targID := greasego.GetTargetId(filterConfig.Target)
+	if targID == 0 {
 		return errors.New("target does not exist")
 	}
 
 	filter := greasego.NewGreaseLibFilter()
 	greasego.AssignFromStruct(filter, filterConfig)
 
-	filter.Target = targId
+	filter.Target = targID
 	greasego.SetFilterValue(filter, greasego.GREASE_LIB_SET_FILTER_TARGET, filter.Target)
 
 	if len(filterConfig.Levels) > 0 {
@@ -151,17 +139,18 @@ func AddLogFilter(filterConfig maestroSpecs.LogFilter) error {
 	return nil
 }
 
+//DeleteLogFilter is used to remove an existing log filter from the live running logs is maestro
 func DeleteLogFilter(filterConfig maestroSpecs.LogFilter) error {
 	fmt.Printf("in DeleteLogFilter\n")
-	targId := greasego.GetTargetId(filterConfig.Target)
-	if targId == 0 {
+	targID := greasego.GetTargetId(filterConfig.Target)
+	if targID == 0 {
 		return errors.New("target does not exist")
 	}
 
 	filter := greasego.NewGreaseLibFilter()
 	greasego.AssignFromStruct(filter, filterConfig)
 
-	filter.Target = targId
+	filter.Target = targID
 	greasego.SetFilterValue(filter, greasego.GREASE_LIB_SET_FILTER_TARGET, filter.Target)
 
 	if len(filterConfig.Levels) > 0 {
@@ -186,7 +175,7 @@ func DeleteLogFilter(filterConfig maestroSpecs.LogFilter) error {
 // Functions to process the parameters which are changed
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Function to process Target config change
+//FileConfigChange Function to process Target config change
 func (inst *logManagerInstance) FileConfigChange(fieldchanged string, futvalue interface{}, curvalue interface{}, index int) {
 	fmt.Printf("FileConfigChange: %s old:%v new:%v\n", fieldchanged, curvalue, futvalue)
 
@@ -203,7 +192,7 @@ func (inst *logManagerInstance) FileConfigChange(fieldchanged string, futvalue i
 	}
 }
 
-//Function to process Target config change
+//NameConfigChange Function to process Target config change
 func (inst *logManagerInstance) NameConfigChange(fieldchanged string, futvalue interface{}, curvalue interface{}, index int) {
 	fmt.Printf("NameConfigChange: %s old:%v new:%v\n", fieldchanged, curvalue, futvalue)
 
@@ -220,7 +209,7 @@ func (inst *logManagerInstance) NameConfigChange(fieldchanged string, futvalue i
 	}
 }
 
-//Function to process filter config change
+//FiltersConfigChange Function to process filter config change
 func (inst *logManagerInstance) FiltersConfigChange(fieldchanged string, futvalue interface{}, curvalue interface{}, index int) {
 	fmt.Printf("FiltersConfigChange: %s old:%v new:%v\n", fieldchanged, curvalue, futvalue)
 
@@ -256,29 +245,30 @@ func (inst *logManagerInstance) FiltersConfigChange(fieldchanged string, futvalu
 		log.MaestroWarnf("FiltersConfigChange:Unknown field: %s: old:%v new:%v\n", fieldchanged, curvalue, futvalue)
 	}
 }
+
+//ApplyChanges this make the filter changes active
 func (inst *logManagerInstance) ApplyChanges() {
 
 	fmt.Println("Entering ApplyChanges()")
 	filterslen := len(inst.logConfig[0].Filters)
 
-	// verify we have an existing target because we don't currently support adding new targets
-	/*if len(inst.logConfig) <= index {
-		fmt.Printf("FiltersConfigChange: No targets exist.  Adding new targets is not supported.\n")
-		return
-	}*/
-	targId := greasego.GetTargetId(inst.logConfig[0].File)
-	if targId == 0 {
+	//get the target id
+	targID := greasego.GetTargetId(inst.logConfig[0].File)
+
+	//make sure its legit
+	if targID == 0 {
 		fmt.Printf("FiltersConfigChange: Target ID not found for Target Name: %s\n", inst.logConfig[0].Name)
 		return
 	}
-	fmt.Println("targId=", targId)
+
+	fmt.Println("targId=", targID)
 
 	// if the existing target at index has a name and if the incoming filter specifies a target name,
 	// then verify the names match
 	if len(inst.logConfig[0].Name) > 0 {
 		for i := 0; i < filterslen; i++ {
-			target_name := inst.logConfig[0].Filters[i].Target
-			if len(target_name) > 0 && (inst.logConfig[0].Name != target_name) {
+			targetName := inst.logConfig[0].Filters[i].Target
+			if len(targetName) > 0 && (inst.logConfig[0].Name != targetName) {
 				fmt.Printf("FiltersConfigChange: filter.Target.Name doesn't match existing target at index 0\n")
 				return
 			}
@@ -304,78 +294,12 @@ func (inst *logManagerInstance) ApplyChanges() {
 	}
 }
 
-//Function to process format config change
+//FormatConfigChange Function to process format config change
 func (inst *logManagerInstance) FormatConfigChange(fieldchanged string, futvalue interface{}, curvalue interface{}, index int) {
 	fmt.Printf("FormatConfigChange: %s old:%v new:%v\n", fieldchanged, curvalue, futvalue)
 }
 
-//Function to process opts config change
+//OptsConfigChange Function to process opts config change
 func (inst *logManagerInstance) OptsConfigChange(fieldchanged string, futvalue interface{}, curvalue interface{}, index int) {
 	fmt.Printf("OptsConfigChange: %s old:%v new:%v\n", fieldchanged, curvalue, futvalue)
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Monitor for ConfigChangeHook
-//////////////////////////////////////////////////////////////////////////////////////////
-type CommitConfigChangeHook struct {
-	//struct implementing CommitConfigChangeHook intf
-}
-
-var configApplyRequestChan chan bool = nil
-
-//This is the go routine which waits on jobConfigApplyRequestChan and when it
-//receives an updated config it submits the config and sets up the interfaces based
-//on new configuration
-func ConfigApplyHandler(jobConfigApplyRequestChan <-chan bool) {
-	fmt.Printf("in ConfigApplyHandler\n")
-	/*	for applyChange := range jobConfigApplyRequestChan {
-		fmt.Printf("ConfigApplyHandler::Received a apply change message: %v\n", applyChange)
-		if applyChange {
-			instance = GetInstance()
-			fmt.Printf("ConfigApplyHandler::Processing apply change: %v\n", instance.CurrConfigCommit.ConfigCommitFlag)
-			instance.submitConfigAndSync(instance.logConfig)
-			//Setup the intfs using new config
-			instance.setupInterfaces()
-			instance.CurrConfigCommit.ConfigCommitFlag = false
-			instance.CurrConfigCommit.LastUpdateTimestamp = time.Now().Format(time.RFC850)
-			instance.CurrConfigCommit.TotalCommitCountFromBoot = instance.CurrConfigCommit.TotalCommitCountFromBoot + 1
-			//Now write out the updated commit config
-			err := instance.ddbConfigClient.Config(DDB_LOG_CONFIG_COMMIT_FLAG).Put(&instance.CurrConfigCommit)
-			if err == nil {
-				fmt.Printf("Updating commit config object to devicedb succeeded.\n")
-			} else {
-				fmt.Printf("Unable to update commit config object to devicedb\n")
-			}
-		}
-	}*/
-}
-
-// ChangesStart is called before reporting any changes via multiple calls to SawChange. It will only be called
-// if there is at least one change to report
-func (cfgHook CommitConfigChangeHook) ChangesStart(configgroup string) {
-	fmt.Printf("CommitChangeHook:ChangesStart: %s\n", configgroup)
-}
-
-// SawChange is called whenever a field changes. It will be called only once for each field which is changed.
-// It will always be called after ChangesStart is called
-// If SawChange return true, then the value of futvalue will replace the value of current value
-func (cfgHook CommitConfigChangeHook) SawChange(configgroup string, fieldchanged string, futvalue interface{}, curvalue interface{}, index int) (acceptchange bool) {
-	fmt.Printf("CommitChangeHook:SawChange: %s:%s old:%v new:%v index:%d\n", configgroup, fieldchanged, curvalue, futvalue, index)
-	/*instance = GetInstance()
-	switch fieldchanged {
-	case "ConfigCommitFlag":
-	case "LastUpdateTimestamp":
-	case "TotalCommitCountFromBoot":
-	default:
-		fmt.Printf("\nCommitChangeHook:Unknown field: %s: old:%v new:%v\n", fieldchanged, curvalue, futvalue)
-	}*/
-	return false //return false as we would apply only those we successfully processed
-}
-
-// ChangesComplete is called when all changes for a specific configgroup tagname
-// If ChangesComplete returns true, then all changes in that group will be assigned to the current struct
-func (cfgHook CommitConfigChangeHook) ChangesComplete(configgroup string) (acceptallchanges bool) {
-	fmt.Printf("CommitChangeHook:ChangesComplete: %s\n", configgroup)
-
-	return false //return false as we would apply only those we successfully processed
 }
