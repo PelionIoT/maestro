e1a6283a743512c4972df8b25b635a9bf84165b1
psppalash <palash.manishkumarshah@arm.com>
2020-08-26 19:32:13 -0500

    add wpa-connect go package
    Add dependency: wpa-connect  https://github.com/armPelionEdge/wpa-connect 10297d26dfc8f2265ce920c7b51f849998f658bf    wpa-connect package is used by wifi manager in maestro networking to send commands to wpa-supplicant over dbus

---
+++ vendor/github.com/armPelionEdge/wpa-connect/wpa-connect.go
@@ -0,0 +1,268 @@
+/*
+ Copyright (c) 2020 ARM Limited and affiliates.
+ Copyright (c) 2016 Mark Berner
+ SPDX-License-Identifier: MIT
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to
+ deal in the Software without restriction, including without limitation the
+ rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+package wpaconnect
+
+import (
+	"errors"
+
+	"fmt"
+	"github.com/godbus/dbus"
+	"github.com/armPelionEdge/wpa-connect/internal/log"
+	"github.com/armPelionEdge/wpa-connect/internal/wpa_cli"
+	"github.com/armPelionEdge/wpa-connect/internal/wpa_dbus"
+	"net"
+	"time"
+)
+
+func (self *connectManager) Connect(ssid string, password string, timeout time.Duration) (connectionInfo ConnectionInfo, e error) {
+	self.deadTime = time.Now().Add(timeout)
+	self.context = &connectContext{}
+	self.context.scanDone = make(chan bool)
+	self.context.connectDone = make(chan bool)
+	if wpa, err := wpa_dbus.NewWPA(); err == nil {
+		wpa.WaitForSignals(self.onSignal)
+		wpa.AddSignalsObserver()
+		if wpa.ReadInterface(self.NetInterface); wpa.Error == nil {
+			iface := wpa.Interface
+			iface.AddSignalsObserver()
+			self.context.phaseWaitForScanDone = true
+			go func() {
+				time.Sleep(self.deadTime.Sub(time.Now()))
+				self.context.scanDone <- false
+				self.context.error = errors.New("timeout")
+			}()
+			if iface.Scan(); iface.Error == nil {
+				// Wait for scan_example done
+				if <-self.context.scanDone; self.context.error == nil {
+					if iface.ReadBSSList(); iface.Error == nil {
+						// Look for target BSS
+						var bssFound = false
+						for _, bss := range iface.BSSs {
+							if bss.ReadSSID(); bss.Error == nil {
+								if ssid == bss.SSID {
+									bssFound = true
+									if err := self.connectToBSS(&bss, iface, password); err == nil {
+										// Wait for connection
+										cli := wpa_cli.WPACli{NetInterface: self.NetInterface}
+										if err := cli.SaveConfig(); err == nil {
+											connectionInfo = ConnectionInfo{NetInterface: self.NetInterface, SSID: ssid,
+												IP4: self.context.ip4, IP6: self.context.ip6}
+										} else {
+											e = err
+										}
+									} else {
+										e = err
+									}
+									break
+								}
+							} else {
+								e = bss.Error
+							}
+						}
+						if !bssFound {
+							e = errors.New("ssid_not_found")
+						}
+					} else {
+						e = iface.Error
+					}
+				} else {
+					e = self.context.error
+				}
+			} else {
+				e = wpa.Error
+			}
+			iface.RemoveSignalsObserver()
+		} else {
+			e = wpa.Error
+		}
+		wpa.RemoveSignalsObserver()
+		wpa.StopWaitForSignals()
+	} else {
+		e = err
+	}
+	return
+}
+
+func (self *connectManager) Disconnect() (e error) {
+	if wpa, err := wpa_dbus.NewWPA(); err == nil {
+		if wpa.ReadInterface(self.NetInterface); wpa.Error == nil {
+			iface := wpa.Interface
+			if iface.RemoveAllNetworks(); iface.Error == nil {
+				e = nil
+			}
+		} else {
+			e = wpa.Error
+		}
+	} else {
+		e = err
+	}
+	return
+}
+
+func (self *connectManager) connectToBSS(bss *wpa_dbus.BSSWPA, iface *wpa_dbus.InterfaceWPA, password string) (e error) {
+	addNetworkArgs := map[string]dbus.Variant{
+		"ssid": dbus.MakeVariant(bss.SSID),
+		"psk":  dbus.MakeVariant(password)}
+	if iface.AddNetwork(addNetworkArgs); iface.Error == nil {
+		network := iface.NewNetwork
+		self.context.phaseWaitForInterfaceConnected = true
+		go func() {
+			time.Sleep(self.deadTime.Sub(time.Now()))
+			self.context.connectDone <- false
+			self.context.error = errors.New("timeout")
+		}()
+		if network.Select(); network.Error == nil {
+			if connected := <-self.context.connectDone; self.context.error == nil {
+				if connected {
+					if err := self.readNetAddress(); err == nil {
+					} else {
+						e = err
+					}
+				} else {
+					if iface.ReadDisconnectReason(); iface.Error == nil {
+						e = errors.New(fmt.Sprintf("connection_failed, reason=%d", iface.DisconnectReason))
+					} else {
+						e = errors.New("connection_failed")
+					}
+				}
+			} else {
+				e = self.context.error
+			}
+		} else {
+			e = network.Error
+		}
+	} else {
+		e = iface.Error
+	}
+	return
+}
+
+func (self *connectManager) onSignal(wpa *wpa_dbus.WPA, signal *dbus.Signal) {
+	log.Log.Debug(signal.Name, signal.Path)
+	switch signal.Name {
+	case "fi.w1.wpa_supplicant1.Interface.BSSAdded":
+	case "fi.w1.wpa_supplicant1.Interface.BSSRemoved":
+		break
+	case "fi.w1.wpa_supplicant1.Interface.ScanDone":
+		self.processScanDone(wpa, signal)
+	case "fi.w1.wpa_supplicant1.Interface.PropertiesChanged":
+		log.Log.Debug(signal.Name, signal.Path, signal.Body)
+		self.processInterfacePropertiesChanged(wpa, signal)
+	default:
+		log.Log.Debug(signal.Name, signal.Path, signal.Body)
+	}
+}
+
+func (self *connectManager) readNetAddress() (e error) {
+	if netIface, err := net.InterfaceByName(self.NetInterface); err == nil {
+		for time.Now().Before(self.deadTime) && !self.context.hasIP() {
+			if addrs, err := netIface.Addrs(); err == nil {
+				for _, addr := range addrs {
+					if ip, _, err := net.ParseCIDR(addr.String()); err == nil {
+						if self.context.ip4 == nil {
+							self.context.ip4 = ip.To4()
+							continue
+						}
+						if self.context.ip6 == nil {
+							self.context.ip6 = ip.To16()
+							continue
+						}
+					} else {
+						e = err
+						return
+					}
+				}
+			} else {
+				e = err
+			}
+			time.Sleep(time.Millisecond * 500)
+		}
+		if !self.context.hasIP() {
+			e = errors.New("address_not_allocated")
+		}
+	} else {
+		e = err
+	}
+	return
+}
+
+func (self *connectManager) processScanDone(wpa *wpa_dbus.WPA, signal *dbus.Signal) {
+	log.Log.Debug("processScanDone")
+	if self.context.phaseWaitForScanDone {
+		self.context.phaseWaitForScanDone = false
+		self.context.scanDone <- true
+	}
+}
+
+func (self *connectManager) processInterfacePropertiesChanged(wpa *wpa_dbus.WPA, signal *dbus.Signal) {
+	log.Log.Debug("processInterfacePropertiesChanged")
+	log.Log.Debug("phaseWaitForInterfaceConnected", self.context.phaseWaitForInterfaceConnected)
+	if self.context.phaseWaitForInterfaceConnected {
+		if len(signal.Body) > 0 {
+			properties := signal.Body[0].(map[string]dbus.Variant)
+			if stateVariant, hasState := properties["State"]; hasState {
+				if state, ok := stateVariant.Value().(string); ok {
+					log.Log.Debug("State", state)
+					if state == "completed" {
+						self.context.phaseWaitForInterfaceConnected = false
+						self.context.connectDone <- true
+						return
+					} else if state == "disconnected" {
+						self.context.phaseWaitForInterfaceConnected = false
+						self.context.connectDone <- false
+						return
+					}
+				}
+			}
+		}
+	}
+}
+
+func (self *connectContext) hasIP() bool {
+	return self.ip4 != nil && self.ip6 != nil
+}
+
+type ConnectionInfo struct {
+	NetInterface string
+	SSID         string
+	IP4          net.IP
+	IP6          net.IP
+}
+
+type connectContext struct {
+	phaseWaitForScanDone           bool
+	phaseWaitForInterfaceConnected bool
+	scanDone                       chan bool
+	connectDone                    chan bool
+	ip4                            net.IP
+	ip6                            net.IP
+	error                          error
+}
+
+type connectManager struct {
+	context      *connectContext
+	deadTime     time.Time
+	NetInterface string
+}
+
+var (
+	ConnectManager = &connectManager{NetInterface: "wlan0"}
+)
